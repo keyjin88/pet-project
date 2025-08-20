4169f90b8bf2ebfd0f107ca9f57d4d6a04c9d2d3
AV <keyjin88@gmail.com>
2025-08-20 22:10:19 +0300

    Initial commit

---
+++ src/test/kotlin/ru/vavtech/petproject/PetProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vavtech.petproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
